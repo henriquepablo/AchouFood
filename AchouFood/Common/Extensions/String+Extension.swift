08d91f26a7fef86db3ca0173d295abac733b2a40
henriquepablo <pablo269704@gmail.com>
2025-11-11 18:17:47 -0300

    chore: padronizando projeto

---
+++ AchouFood/Common/Extensions/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extension.swift
+//  AchouFood
+//
+//  Created by pablo henrique on 11/11/25.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}

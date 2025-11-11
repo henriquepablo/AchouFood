0f35af50a6d58fb95855df6b7522e1992de7464f
henriquepablo <pablo269704@gmail.com>
2025-11-11 17:11:22 -0300

    Initial Commit

---
+++ AchouFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AchouFood
+//
+//  Created by pablo henrique on 11/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AchouFoodTests/AchouFoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  AchouFoodTests.swift
+//  AchouFoodTests
+//
+//  Created by pablo henrique on 11/11/25.
+//
+
+import Testing
+@testable import AchouFood
+
+struct AchouFoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

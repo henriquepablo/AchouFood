f971aa4392890f3908d22b321070c7a2de690ad6
henriquepablo <pablo269704@gmail.com>
2025-11-13 11:28:20 -0300

    feat: create first view

--- AchouFood/Scenes/HomeDelivery/HomeDeliveryView.swift
+++ AchouFood/Scenes/HomeDelivery/HomeDeliveryView.swift
@@ -7,9 +7,29 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
+struct Constants {
+    static let headerHeight = 36
+    static let cornerRadius = 20.0
+    
+}
 
 class HomeDeliveryView: UIView {
+    
+    private lazy var headerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var backgroudView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Color.gray100
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,14 +45,26 @@ class HomeDeliveryView: UIView {
 extension HomeDeliveryView: ViewCodeProtocol {
     
     func setViewHierarchy() {
-        
+        addSubview(headerView)
+        addSubview(backgroudView)
     }
     
     func setViewConstraints() {
+        headerView.snp.makeConstraints{ make in
+            make.height.equalTo(Constants.headerHeight)
+            make.top.leading.trailing.equalToSuperview().inset(Metrics.medium)
+        }
         
+        backgroudView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom).offset(Metrics.medium)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
     
     func setViewConfigs() {
         backgroundColor = Color.redDark
+        backgroudView.layer.cornerRadius = Constants.cornerRadius
+        backgroudView.layer.masksToBounds = true
+        backgroudView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
 }
